a33700e1c6a881a6986baf51753a9108254eb617
Anita Beauchamp <anitabeauchamp@Anitas-MBP.cfl.rr.com>
2021-07-19 15:06:42 -0400

    Refactored code to use new ChallengeTracker class

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import datetime as dt
-import functions as fu
+import challenge_tracker as ct
 
 if __name__ == '__main__':
-    DURATION = dt.timedelta(days=100)
-    start_date = dt.date.fromisoformat(input('Enter start date: '))
-    end_date = fu.get_end_date(start_date, DURATION)
-    current_day = fu.get_current_day(start_date)
-    days_remaining = fu.get_days_left(start_date, DURATION)
+    start_date = input('Enter start date in ISO format: ')
+    duration = int(input('Enter challenge duration in days: '))
 
-    print(f'Your start date is {start_date.strftime("%b %d %Y")}')
-    print(f'Your end date is {end_date.strftime("%b %d %Y")}')
-    print(f'You are currently on Day {current_day.days}')
-    print(f'You have {days_remaining.days} days left')
+    new_challenge = ct.ChallengeTracker(start_date, duration)
+
+    print()
+    print("================== Your Challenge Information ====================")
+    print(f'Your start date is {new_challenge.start_date.strftime("%b %d, %Y")}')
+    print(f'Your end date is {new_challenge.end_date.strftime("%b %d, %Y")}')
+    print(f'You are currently on Day {new_challenge.currentDay()}')
+    print("===================================================================")
