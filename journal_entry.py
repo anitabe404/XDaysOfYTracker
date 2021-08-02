6f314cb198c73e6e5bf1836ab73a85a177c45e1b
Anita Beauchamp <anitabeauchamp@Anitas-MBP.cfl.rr.com>
2021-08-02 02:48:14 -0400

    Add load class method. Update test cases.

--- journal_entry.py
+++ journal_entry.py
@@ -1,11 +1,13 @@
 import datetime as dt
 
 class Entry:
-    def __init__(self, content=None):
-        self.date_created = dt.date.today().isoformat()
-        self.last_modified = dt.date.today().isoformat() # Should this be a datetime object?
-        self.content = content or "Today's Progress: \nThoughts: \nLink to work: "
+    VALID_KEYS = ['content', 'date_created', 'last_modified']
 
+    def __init__(self, content:str=None, date_created=None, last_modified=None):
+        self.content = content or "Today's Progress: \nThoughts: "
+        self.date_created = date_created or dt.datetime.today().isoformat()
+        self.last_modified = last_modified or dt.datetime.today().isoformat()
+        
     def modify(self, new_content: str) -> None:
         self.content = new_content
         self.last_modified = dt.datetime.now().isoformat()
@@ -16,3 +18,35 @@ class Entry:
                     'date_created': self.date_created,
                     'last_modified': self.last_modified
         }
+    
+    @classmethod
+    def load(cls, imported_data:dict):
+        if not isinstance(imported_data,dict):
+            return None
+            
+        # content_fail_flag = False
+        iso_fail_flag = False
+        last_mod_fail_flag = False
+        invalid_key_fail_flag = False
+
+        # Check that keys are valid.
+        imported_keys = list(imported_data.keys())
+        imported_keys.sort()
+        if imported_keys != Entry.VALID_KEYS:
+            invalid_key_fail_flag = True
+            print(f"Entry - Invalid key: {invalid_key_fail_flag}.")
+            return None
+
+        content = imported_data['content']
+        date_created = imported_data['date_created']
+        last_modified = imported_data['last_modified']
+        try:
+            dc = dt.datetime.fromisoformat(date_created)
+            lm = dt.datetime.fromisoformat(last_modified)
+            last_mod_fail_flag = lm < dc
+        except:
+            iso_fail_flag = True
+            print("Dates are not in iso format.")
+        
+        return Entry(content, date_created, last_modified) \
+                if not (iso_fail_flag or last_mod_fail_flag) else None          
--- test_journal_entry.py
+++ test_journal_entry.py
@@ -1,45 +1,63 @@
 import unittest
 import datetime as dt
 import challenge_tracker as trckr
-import journal_entry as je
+from journal_entry import Entry
 
 class TestJournalEntry(unittest.TestCase):
-    def test_init(self):
-        today = dt.date.today()
-        start_date = (today - dt.timedelta(days=4)).isoformat()
-        duration = 10
-        tracker = trckr.ChallengeTracker(start_date, duration)
-        day = tracker.getDayFromDate(today.isoformat())
-        new_entry = je.Entry(day, today.isoformat())
-        self.assertEqual(new_entry.date_created, today.isoformat())
-        self.assertEqual(new_entry.last_modified, today.isoformat())
-        self.assertEqual(new_entry.content, "Today's Progress: \nThoughts: \nLink to work: ")
+    def test_init_no_arguments(self):
+        today = dt.datetime.now().isoformat()
+        new_entry = Entry()
+        print(f"DC: {new_entry.date_created}\nLM:{new_entry.last_modified}")
+        # self.assertEqual(new_entry.date_created, today)
+        # self.assertEqual(new_entry.last_modified, today)
+        self.assertEqual(new_entry.content, "Today's Progress: \nThoughts: ")
+    
+    def test_init_with_arguments(self):
+        date_created = "2021-07-01"
+        last_modified = "2021-07-07"
+        content = "A journal entry"
+        entry = Entry(content,date_created,last_modified)
+        self.assertEqual(entry.content, content)
+        self.assertEqual(entry.date_created, date_created)
+        self.assertEqual(entry.last_modified, last_modified)
 
-    def test_modify(self):
-        today = dt.date.today()
-        start_date = (today - dt.timedelta(days=4)).isoformat()
-        duration = 10
-        tracker = trckr.ChallengeTracker(start_date, duration)
-        day = tracker.getDayFromDate(today.isoformat())
-        new_entry = je.Entry(day, today.isoformat())
+    def test_modify(self): 
+        creation_date = "2021-07-05"
+        entry = Entry(date_created=creation_date)
         new_content = "Some random text."
-        new_entry.modify(new_content)
-        self.assertEqual(new_entry.content, new_content)
-        last_modified = dt.datetime.fromisoformat(new_entry.last_modified)
-        date_created = dt.datetime.fromisoformat(new_entry.date_created)
-        self.assertGreater(last_modified, date_created)
+        entry.modify(new_content)
+        self.assertEqual(entry.content, new_content)
+        last_modified = dt.datetime.fromisoformat(entry.last_modified)
+        self.assertGreater(last_modified, dt.datetime.fromisoformat(creation_date))
 
     def test_asJSONObj(self):
-        today = dt.date.today()
-        start_date = (today - dt.timedelta(days=4)).isoformat()
-        duration = 10
-        tracker = trckr.ChallengeTracker(start_date, duration)
-        day = tracker.getDayFromDate(today.isoformat())
-        journal_entry = je.Entry(day, today.isoformat())
-        journal_dict = journal_entry.asJSONObj()
-        self.assertEqual(journal_entry.date_created, journal_dict['date_created'])
-        self.assertEqual(journal_entry.last_modified, journal_dict['last_modified'])
-        self.assertEqual(journal_entry.content, journal_dict['content'])
-        j_keys = list(journal_dict.keys())
+        entry = Entry("My content goes here")
+        entry.modify("My content goes here with new stuff")
+        json_entry = entry.asJSONObj()
+        self.assertEqual(entry.date_created, json_entry['date_created'])
+        self.assertEqual(entry.last_modified, json_entry['last_modified'])
+        self.assertEqual(entry.content, json_entry['content'])
+        j_keys = list(json_entry.keys())
         j_keys.sort()
         self.assertEqual(j_keys,['content', 'date_created', 'last_modified'])
+    
+    def test_load(self):
+        imported_entry = {
+                "content": "I think I have the journal feature working!!!!!!!!! I'm about to lose my mind. Added some more text.",
+                "date_created": "2021-07-27",
+                "last_modified": "2021-07-28T01:21:57.898677"
+            }
+        entry = Entry.load(imported_entry)
+        content = imported_entry['content']
+        date_created = imported_entry['date_created']
+        last_modified = imported_entry['last_modified']
+        self.assertEqual(entry.content,content)
+        self.assertEqual(entry.date_created, date_created)
+        self.assertEqual(entry.last_modified, last_modified)
+    
+    def test_load_bad_data(self):
+        bad_data = 9
+        entry = Entry.load(bad_data)
+        self.assertIsNone(entry)
+
+TestJournalEntry().test_init_no_arguments()
